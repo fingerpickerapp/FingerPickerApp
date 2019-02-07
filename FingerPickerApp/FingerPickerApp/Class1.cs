8e2233364e6fbf6624770ed46a0e5970bee86bfd
Albara <almedspani@gmail.com>
2019-02-07 17:23:38 +0000

    Touch

---
+++ FingerPickerApp/FingerPickerApp/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FingerPickerApp
+{
+    public class Class1
+    {
+        String me = "1234";
+        String ok = "123455";
+        String aksa = "hi preena";
+    }
+}
---
+++ aksa_fp/FP/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace FP
+{
+    [Activity(Label = "FP5", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@android:style/Theme.Black.NoTitleBar")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            System.Diagnostics.Debug.WriteLine("Am I here?");
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += (a, b) =>
+             {
+                 Intent secondActivity = new Intent(this, typeof(touch));
+                 StartActivity(secondActivity);
+             };
+
+
+
+
+        }
+    }
+}
+

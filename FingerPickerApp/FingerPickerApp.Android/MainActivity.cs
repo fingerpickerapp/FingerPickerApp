41c9f045fc11403a8fe756c0ecd11cc983afcf6e
Albara Almeda <almedaspani@gmail.com>
2019-02-18 13:26:17 +0000

    Finger Class added

--- FingerPickerApp/FingerPickerApp.Android/MainActivity.cs
+++ FingerPickerApp/FingerPickerApp.Android/MainActivity.cs
@@ -1,11 +1,9 @@
-﻿using System;
-
+﻿
+using System;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using FingerPickerApp;
 
 namespace FingerPickerApp.Droid
 {
@@ -20,6 +18,10 @@ namespace FingerPickerApp.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Finger finger = new Finger(10,10,10,"Black");
+
+            Console.WriteLine("finger picker X = " + finger.getFingerX());
         }
     }
 }
---
+++ FingerPickerApp/FingerPickerApp/Finger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace FingerPickerApp
+{
+    public class Finger
+    {
+        private int fingerId;
+        private int fingerX;
+        private int fingerY;
+        private String fingerColour;
+
+        public Finger(int fingerId, int fingerX, int fingerY, String fingerColour)
+        {
+            this.fingerId = fingerId;
+            this.fingerX = fingerX;
+            this.fingerY = fingerY;
+            this.fingerColour = fingerColour;
+        }
+
+        public void setFingerId(int fingerId)
+        {
+            this.fingerId = fingerId;
+        }
+
+        public int getFingerId()
+        {
+            return fingerId;
+        }
+
+        public void setFingerX(int fingerX)
+        {
+            this.fingerX = fingerX;
+        }
+
+        public int getFingerX()
+        {
+            return fingerX;
+        }
+
+        public void setFingerY(int fingerY)
+        {
+            this.fingerY = fingerY;
+        }
+
+        public int getFingerY()
+        {
+            return fingerY; 
+        }
+
+        public void setFingerColour(String fingerColour)
+        {
+            this.fingerColour = fingerColour;
+        }
+
+        public String getFingerColour()
+        {
+            return fingerColour;
+        }
+
+    }
+}
